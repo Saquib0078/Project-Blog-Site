18a4eddb9c33990ed086cb727a94b5b41720f20b
yachika03 <yachikamehta03@gmail.com>
2022-09-08 19:45:20 +0530

    final

--- src/middlwares/middlware.js
+++ src/middlwares/middlware.js
@@ -17,69 +17,25 @@ try{
 
 }
 
-// const authorise =async function(req, res, next) {
-//     try {
-//       let token = req.headers["x-api-key"]
-//       let decodeToken = jwt.verify(token,"blog-site-project-01") 
-      
-//       let userToBeModified = req.params.authorId
-//       let userLoggedIn = decodeToken.authorId
-//       if(userLoggedIn != userToBeModified)
-//       return res.status(400).send({status:false, msg:"you are not authorize for changes"})
-//       next()
-//     } catch (error) {
-//         return res.status(400).send(error.message);
-//     }
-   
-// } 
 const authorise =async function(req, res, next) {
-try{
-    let token = req.headers["x-api-key"];
-    
-if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
-let decodedToken = jwt.verify(token, "blog-site-project-01");
-let userTobeModified =req.query.authorId
-let userLoggedIn = decodedToken.authorId
-
-if (!decodedToken)
-   return res.status(401).send({ status: false, msg: "token is invalid" });
-   if(userTobeModified != userLoggedIn) return res.status(403).send({status:false,msg:"You are not Authorized"})
+    try {
+      let token = req.headers["x-api-key"]
+      let decodeToken = jwt.verify(token,"blog-site-project-01")
+      
+      let userToBeModified = req.params.blogId
+      let userLoggedIn = await blogModel.findById(userToBeModified)
+    //   console.log(result)
+       userLoggedIn.authorId = decodeToken.authorId
+      if( userLoggedIn.authorId != decodeToken.authorId
+ )
+      return res.status(400).send({status:false, msg:"you are not authorize for changes"})
+      next()
+    } catch (error) {
+        return res.status(400).send(error.message);
+    }
    
-   next()
-  }catch(error){
-    res.status(500).send({msg: error.message})
-  }
-}
-    
+} 
+
 module.exports.authenticate = authenticate
 module.exports.authorise = authorise
 
-
-
-
-
-
-
-
-// const authenticate = function(req, res, next) {
-// let token = req.headers["x-api-key"]
-//     if(!token) return res.status(400).send({status:false, msg:"token is needed"})
-// let validToken = jwt.verify(token,"blog-site-project-01")
-//     if (!validToken) return res.status(400).send({status: false, msg: "token is Invalid"}) 
-//     req.decodedToken=validToken
-// res.headers["x-api-key",token]
-//  next()
-// }
-
-
-// const authorise = function(req, res, next) {
-
-// req.id =  req.params.blogId;
-// let loginUser = req.decodedToken.authorId
-// if(loginUser != req.id) return res.status(403).send({status:false, msg: "not authorize"})
-
-//  next()
-// }
-    
-// module.exports.authenticate = authenticate
-// module.exports.authorise = authorise
